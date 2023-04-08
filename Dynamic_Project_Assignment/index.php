1a6ad1324cb6fa5fc83fc0ec321fdc986565f8c9
Phonginhere <tranhaiphong2016fpt@gmail.com>
2023-04-08 12:15:31 +0700

    update

--- Dynamic_Project_Assignment/index.php
+++ Dynamic_Project_Assignment/index.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="./css/index.css">
 
 <body>
+    <?php include_once('./navbar.php'); ?>
     <!-- Carousel -->
     <div id="demo" class="carousel slide" data-bs-ride="carousel">
 
@@ -24,8 +25,12 @@
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <img src="./img/background_index.jpg" alt="Los Angeles" class="d-block" width="100%" height="500">
-                <div class="centered_title"><h1>Main title</h1></div>
-                <div class="centered_desc"><h3>Description</h3></div>
+                <div class="centered_title">
+                    <h1>Main title</h1>
+                </div>
+                <div class="centered_desc">
+                    <h3>Description</h3>
+                </div>
                 <div class="centered_button"><a href="#" target="_blank">Open Link</a></div>
             </div>
             <div class="carousel-item">
@@ -45,67 +50,35 @@
         </button>
     </div>
 
-    <div class="container-fluid mt-3">
-        <h3>Carousel Example</h3>
+    <!-- <div class="container-fluid mt-3">
+        <h3>67 wild camping, swimming, scenic nature sites</h3>
         <p>The following example shows how to create a basic carousel with indicators and controls.</p>
-    </div>
+    </div> -->
     <!-- <div class="p-5 bg-primary text-white text-center">
   <h1>My First Bootstrap 5 Page</h1>
   <p>Resize this responsive page to see the effect!</p>
 </div> -->
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"
-                aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <a class="navbar-brand" href="#">Hidden brand</a>
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Information</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Availability</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Reviews</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Reviews</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Register</a>
-                    </li>
-                    <!-- <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li> -->
-                </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
 
-            </div>
-    </nav>
 
     <div class="container mt-5">
         <div class="row">
             <div class="col-sm-4">
-                <h2>About Me</h2>
-                <h5>Photo of me:</h5>
-                <div class="fakeimg">Fake Image</div>
-                <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+                <h2>67 wild camping, swimming, scenic nature sites</h2>
+                <h5>Nantcol Waterfalls</h5>
+                <h6 class="text-info">Nantcol Waterfalls</h6>
+                <img src="./img/nantcol-waterfalls.jpg" alt="" class="fakeimg">
+                <ul class="list-group cProductsList">
+                    <li class="d-flex"><button> <a class="nav-link" href="#">Features</a></button>
+                        <button> <a class="nav-link" href="#">All-time</a></button>
+                    </li>
+                </ul>
+                <p>Camping, touring and glamping in lovely Snowdonia with a fun river to swim and fish in, great for walkers.</p>
+                <ul class="list-group cProductsList">
+                    <li class="d-flex"><img src="./img/nantcol-waterfalls.jpg" alt="" style="width:45px; border-radius: 50%;">
+                    <img src="./img/nantcol-waterfalls.jpg" alt="" style="width:45px; border-radius: 50%;">
+                    </li>
+                </ul>
                 <h3 class="mt-4">Some Links</h3>
                 <p>Lorem ipsum dolor sit ame.</p>
                 <ul class="nav nav-pills flex-column">
@@ -125,8 +98,11 @@
                 <hr class="d-sm-none">
             </div>
             <div class="col-sm-8">
-                <h2>TITLE HEADING</h2>
-                <h5>Title description, Dec 7, 2020</h5>
+                <center>
+                    <h2>Direction Map</h2>
+                </center>
+                <iframe src="https://www.google.com/maps/d/embed?mid=16xO6zIWvp8Ac0pMtdnmnkc6EYBu4xWk&ehbc=2E312F" width="100%" height="480"></iframe>
+                <!-- <h5>Title description, Dec 7, 2020</h5>
                 <div class="fakeimg">Fake Image</div>
                 <p>Some text..</p>
                 <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do
@@ -139,14 +115,14 @@
                 <p>Some text..</p>
                 <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do
                     eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                    exercitation ullamco.</p>
+                    exercitation ullamco.</p> -->
             </div>
         </div>
     </div>
 
-    <div class="mt-5 p-4 bg-dark text-white text-center">
-        <p>Footer</p>
-    </div>
+    <?php
+    include_once('./footer.php');
+    ?>
 
 </body>
 
